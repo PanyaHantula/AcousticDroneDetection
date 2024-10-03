8a7f0275153f6a8281656bbd08ad1c8e460c7c44
Panya Hantula <panya@Panyas-MacBook-Air.local>
2024-10-04 02:13:43 +0700

    Test record drone sound. The audio fune is 2 seconed time

--- release/AutoAudioCapture.py
+++ release/AutoAudioCapture.py
@@ -9,47 +9,49 @@ devices = PvRecorder.get_available_devices()
 for device_index in range(len(devices)):
     print(f"CH[{device_index}] : {devices[device_index]}")  
 
-# audio config 
-chunk = 1024  # Record in chunks of 1024 samples
-sample_format = pyaudio.paInt16  # 16 bits per sample
-channels = 2
-fs = 44100  # Record at 44100 samples per second
-seconds = 4
+try:
+    # audio config 
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 1
+    fs = 44100  # Record at 44100 samples per second
+    seconds = 2
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
-# Record
-p = pyaudio.PyAudio()  # Create an interface to PortAudio
+    while True: 
+        print('Recording....')
+        stream = p.open(format=sample_format,
+                        channels=channels,
+                        rate=fs,
+                        frames_per_buffer=chunk,
+                        input=True)
 
-print('Recording')
-stream = p.open(format=sample_format,
-                channels=channels,
-                rate=fs,
-                frames_per_buffer=chunk,
-                input=True)
+        frames = []  # Initialize array to store frames
 
-frames = []  # Initialize array to store frames
+        # Store data in chunks for 3 seconds
+        for i in range(0, int(fs / chunk * seconds)):
+            data = stream.read(chunk)
+            frames.append(data)
 
-# Store data in chunks for 3 seconds
-for i in range(0, int(fs / chunk * seconds)):
-    data = stream.read(chunk)
-    frames.append(data)
+        # Stop and close the stream 
+        stream.stop_stream()
+        stream.close()
 
-# Stop and close the stream 
-stream.stop_stream()
-stream.close()
-# Terminate the PortAudio interface
-p.terminate()
+        # Save the recorded data as a WAV file
+        WaveFileName = "./AudioOutput/Capture-" + str(datetime.datetime.now()) + ".wav"  
+        print(f"save audio: {WaveFileName}")
+        wf = wave.open(WaveFileName, 'wb')
+        wf.setnchannels(channels)
+        wf.setsampwidth(p.get_sample_size(sample_format))
+        wf.setframerate(fs)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+except KeyboardInterrupt:
+    # Terminate the PortAudio interface
+    p.terminate()
 
-print('Finished recording')
 
-# Save the recorded data as a WAV file
-WaveFileName = "/Users/panya/Project-ALL/DroneClassification/AcousticDroneDetection/AudioOutput/Capture-" + str(datetime.datetime.now()) + ".wav"  
-wf = wave.open(WaveFileName, 'wb')
-wf.setnchannels(channels)
-wf.setsampwidth(p.get_sample_size(sample_format))
-wf.setframerate(fs)
-wf.writeframes(b''.join(frames))
-wf.close()
-
+"""
 # playblack Recorde Audio
 import os
 import librosa # type: ignore
@@ -85,3 +87,4 @@ plt.ylabel('Amplitude')
 
 plt.tight_layout(rect=[0, 0, 1, 0.95])
 plt.show()
+"""
